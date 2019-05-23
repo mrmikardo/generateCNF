d94074779e674623c0260eea57bcfc419ecf7f47
mrmikardo <jackwestmore@gmail.com>
2019-05-23 13:31:17 +0100

    Basic working implementation

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # generateCNF
 Python program to generate DIMACS-style CNF files, some of them quite large.
+
+## TODO
+
+ * Using random number generators probably isn't a very good way to generate
+   CNF. There is probably a better, statistically rigorous way to do this...
+ * See TODOs in code. It's ugly.
---
+++ generate_cnf.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import sys
+
+from random import randrange, choice
+
+num_clauses = 100    # default
+
+def gen_clauses(num_vars, num_clauses=100, file_or_stdout="file"):
+    
+    # TODO use a generator!!
+
+    variables = [v for v in range(0, int(num_vars))]
+    variables += [-v for v in range(0, int(num_vars))]
+    print(variables)    # debug
+
+    f = "output.cnf"
+    with open(f, 'w') as ofile:
+        ofile.write("c output.cnf\n")
+        ofile.write("c\n")
+        for i in range(0, num_clauses):
+            cl = gen_clause(variables)
+            ofile.write(str(i) + " ")
+            for j in range(0, len(cl)):
+                ofile.write(str(cl[j]) + " ")
+            ofile.write("\n")
+    ofile.close()
+
+def gen_clause(variables):
+    '''
+    Max length of a clause capped at 5 vars
+
+    TODO: this should be an optional parameter
+    TODO: negative as well as positive literals!
+    '''
+    clause_length = randrange(1, 5+1)
+    clause = []
+    for i in range(0, clause_length):
+        clause.append(choice(variables))
+    return clause
+
+def get_params():
+    '''
+    Returns params: 
+    
+    0: num_vars
+    1: num_clauses (to generate)
+    2: write to stdout or file?
+    '''
+    pass
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: ./generateCNF <number of variables> <OPTIONAL: number of clauses> <OPTIONAL: out file>")
+        sys.exit(1)
+
+    # TODO probably use argparse or similar here
+
+    params = sys.argv[1:]
+    if len(params) == 3:
+        gen_clauses(params[0], params[1], params[2])
+    elif len(params) == 2:
+        gen_clauses(params[0], params[1])
+    else:
+        gen_clauses(params[0])
+
+if __name__ == '__main__': main()
